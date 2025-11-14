c939b328719bcf837ca1e10ef19d633263225b43
AmuroRX-93 <leonyuzhewang@icloud.com>
2025-11-14 17:34:39 +0800

    finish drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TeleOpDriveCommand.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+
+//import org.firstinspires.ftc.teamcode.subsystems.drive.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.drive.MecanumDriveOTOS;
+
+public class TeleOpDriveCommand extends CommandBase {
+    private final MecanumDriveOTOS drive;
+    private final GamepadEx gamepadEx;
+    private final boolean[] isAuto;
+
+    public TeleOpDriveCommand(MecanumDriveOTOS drive, GamepadEx gamepadEx, boolean[] isAuto) {
+        this.drive = drive;
+        this.gamepadEx = gamepadEx;
+        this.isAuto = isAuto;
+        addRequirements(drive);
+    }
+
+    @Override
+    public void execute() {
+        if (!isAuto[0]) {
+            drive.moveRobotFieldRelative(gamepadEx.getLeftY(), gamepadEx.getLeftX(), gamepadEx.getRightX());
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/Solo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/Solo.java
@@ -3,29 +3,18 @@ package org.firstinspires.ftc.teamcode.opmodes.teleops;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.InstantCommand;
-import com.arcrobotics.ftclib.command.WaitCommand;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.bylazar.configurables.annotations.Configurable;
-import com.pedropathing.follower.Follower;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-//import org.firstinspires.ftc.teamcode.commands.IntakeCommand;
-//import org.firstinspires.ftc.teamcode.commands.TeleOpDriveCommand;
-//import org.firstinspires.ftc.teamcode.commands.TransitCommand;
-//import org.firstinspires.ftc.teamcode.commands.TransitCommandWithShooter;
-import org.firstinspires.ftc.teamcode.subsystems.drive.Constants;
+import org.firstinspires.ftc.teamcode.commands.TeleOpDriveCommand;
 import org.firstinspires.ftc.teamcode.subsystems.drive.MecanumDriveOTOS;
-import org.firstinspires.ftc.teamcode.subsystems.intake.Intake;
-import org.firstinspires.ftc.teamcode.subsystems.shooter.Shooter;
-import org.firstinspires.ftc.teamcode.subsystems.shooter.ShooterConstants;
-//import org.firstinspires.ftc.teamcode.subsystems.transit.Transit;
 import org.firstinspires.ftc.teamcode.utils.FunctionalButton;
 
 @Config
@@ -33,13 +22,39 @@ import org.firstinspires.ftc.teamcode.utils.FunctionalButton;
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOpAlpha")
 public class Solo extends CommandOpMode {
     private MecanumDriveOTOS drive;
+    private Telemetry telemetryM;
+    private GamepadEx gamepadEx1;
+    private boolean[] isAuto = {false};
 
     @Override
     public void initialize() {
+        drive = new MecanumDriveOTOS(hardwareMap);
+        gamepadEx1 = new GamepadEx(gamepad1);
 
+
+        drive.setDefaultCommand(new TeleOpDriveCommand(drive, gamepadEx1, isAuto));
+
+
+        new FunctionalButton(
+                () -> gamepadEx1.getButton(GamepadKeys.Button.LEFT_STICK_BUTTON)
+        ).whenPressed(
+                new InstantCommand(() -> drive.reset(0))
+        );
     }
+
     @Override
     public void run() {
+        telemetryM = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+
+        CommandScheduler.getInstance().run();
+
+        telemetry.addData("X", drive.getPose().getX(DistanceUnit.MM));
+        telemetry.addData("Y", drive.getPose().getY(DistanceUnit.MM));
+        telemetry.addData("Heading", drive.getPose().getHeading(AngleUnit.RADIANS));
+        telemetry.addData("YawOffset", drive.getYawOffset());
+        telemetry.update();
+
 
     }
 }
